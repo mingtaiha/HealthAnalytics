daeef6693f6b19a4e04ca8ef12be79588c074ea1
mingtaiha <ming.tai.ha@gmail.com>
2015-12-02 20:26:04 -0500

    Aggregated State Level Stats for Display

---
+++ 4_data_collection/Simulations/Correlated_Gen4_100/Stat_Aggregator.py
@@ -0,0 +1,136 @@
+import pandas as pd
+from math import floor
+from math import ceil
+
+
+
+state_abb = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', \
+			'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', \
+			'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', \
+			'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'PR', \
+			'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', \
+			'WI', 'WY']
+
+
+gender = []
+ethni = []
+
+
+
+# Aggregating the Gender Precentages in the DataFrame
+
+repeat = '_SampleData.csv'
+
+
+print "Doing Aggregation for  Gender"
+
+for state in range(len(state_abb)):
+
+	filename = state_abb[ state ] + repeat
+
+	df = pd.read_csv(filename)
+	df.pop('Unnamed: 0')
+
+	total_value = float(len(df.index))
+	unique = df['Gender'].unique()
+
+	tmpList = []
+
+	for value in unique:
+		count_value = float(df[df['Gender'] == value]['Gender'].count())
+		tmpList.append( count_value / total_value )
+
+	gender.append(tmpList)
+
+df_out = pd.DataFrame(gender, index=state_abb)
+df_out.columns = unique
+df_out.to_csv('Gender_Summary.csv')
+
+
+
+print "Doing Aggregation for  Ethnicity"
+
+for state in range(len(state_abb)):
+
+	filename = state_abb[ state ] + repeat
+
+	df = pd.read_csv(filename)
+	df.pop('Unnamed: 0')
+
+	total_value = float(len(df.index))
+	unique = df['Ethnicity'].unique()
+
+	tmpList = []
+
+	for value in unique:
+		count_value = float(df[df['Ethnicity'] == value]['Ethnicity'].count())
+		tmpList.append( count_value / total_value )
+
+	ethni.append(tmpList)
+
+df_out = pd.DataFrame(ethni, index=state_abb)
+df_out.columns = unique
+df_out.to_csv('Ethnicity_Summary.csv')
+
+predictor = ['Age_years', 'Height', 'Waist', 'Weight', 'BMI', 'WtHR', 'HR', \
+			'Avg_Sys', 'Avg_Dia', 'Tri', 'LDL', 'HDL']
+
+for key in range(len(predictor)):
+
+	continuous = []
+
+	print "Doing Aggregation for ", predictor[ key ]
+
+	curr_prd = predictor[ key ]
+
+	for state in range(len(state_abb)):
+
+		filename = state_abb[ state ] + repeat
+
+		df = pd.read_csv(filename)
+		df.pop('Unnamed: 0')
+
+		total_value = float(len(df.index))
+		
+		min_value = floor(df[ curr_prd ].min())
+		max_value = ceil(df[ curr_prd ].max())
+		average = df[ curr_prd ].mean()
+		std = df[ curr_prd ].std()
+
+		tmpList = []
+
+		tmpList.append(average)
+		tmpList.append(std)
+		tmpList.append(min_value)
+		tmpList.append(max_value)
+
+		slope = (max_value - min_value) / 20
+		bins = [(min_value + (slope * float(i))) for i in range(21)]
+
+		for j in range(20):
+			count_value = float(df[(df[ curr_prd ] >= bins[ j ]) \
+					& (df[ curr_prd ] < bins[ j + 1 ])][ curr_prd ].count())
+
+			tmpList.append( count_value / total_value )
+			
+
+		continuous.append(tmpList)
+
+
+	df_out = pd.DataFrame(continuous, index=state_abb)
+	columns = ['Avg', 'Std', 'Min', 'Max', 'Bin1', 'Bin2', 'Bin3', 'Bin4', 'Bin5', \
+				'Bin6', 'Bin7', 'Bin8', 'Bin9', 'Bin10', 'Bin11', 'Bin12', 'Bin13', \
+				'Bin14', 'Bin15', 'Bin16', 'Bin17', 'Bin18', 'Bin19', 'Bin20']
+	df_out.columns = columns
+
+	if predictor[ key ] == 'Age_years':
+		df_out.to_csv('Age_Summary.csv')
+
+	elif predictor[ key ] == 'Avg_Sys':
+		df_out.to_csv('Sys_Summary.csv')
+
+	elif predictor[ key ] == 'Avg_Dia':
+		df_out.to_csv('Dia_Summary.csv')
+
+	else:
+		df_out.to_csv(predictor[ key ] + '_Summary.csv')
--- 4_data_collection/Simulations/Health_Stat_Verify.py
+++ 4_data_collection/Simulations/Health_Stat_Verify.py
@@ -9,52 +9,35 @@ state_abb = [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', \
 			'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', \
 			'WI', 'WY']
 
-overweight = []
-hypertension = []
-cholesterol = []
+
+gender = []
+ethnicity = []
 
 
+# Aggregating the Gender Precentages in the DataFrame
 
 repeat = '_SampleData.csv'
 
 
 for state in range(len(state_abb)):
 
-	print "Checking health for ", state_abb[ state ]
-	cholesterol_count = 0
-	overweight_count = 0
-	hypertension_count = 0
-
-	fraction_sample_overweight = 0.0
-	fraction_sample_hypertension = 0.0
+	print "Doing Gender Aggregation "
 
 	filename = state_abb[ state ] + repeat
 
 	df = pd.read_csv(filename)
 	df.pop('Unnamed: 0')
 
-	sample_population = len(df.index)
+	sample_population = float(len(df.index))
 
-	overweight_count = len(df[ df['BMI'] >= 25.0 ].index) 
-	fraction_sample_overweight = float(overweight_count) / float(sample_population)
-	overweight.append(fraction_sample_overweight)
+	unique = df['Gender'].unique()
 
-	hypertension_count = len(df[ (df['Avg_Sys'] >= 140.0) | (df['Avg_Dia'] >= 90)].index)
-	fraction_sample_hypertension = float(hypertension_count) / float(sample_population)
-	hypertension.append(fraction_sample_hypertension)
+	tmpList = []
 
+	for value in unique:
+		print value
 
-	for person in range(sample_population):
-
-		ch_tmp = 0.0
-		ch_tmp = df['LDL'][ person ] + df['HDL'][ person ] + (df['Tri'][ person ] / 5)
-
-		if ch_tmp >= 240.0:
-			cholesterol_count += 1
-
-		else:
-			pass
-
+"""
 	cholesterol.append(float(cholesterol_count) / float(sample_population))
 
 df_out = pd.DataFrame(state_abb, index=range(len(state_abb)))
@@ -62,4 +45,4 @@ df_out['Overweight'] = pd.Series(overweight, index=range(len(overweight)))
 df_out['Hypertension'] = pd.Series(hypertension, index=range(len(hypertension)))
 df_out['High_Cholesterol'] = pd.Series(cholesterol, index=range(len(cholesterol)))
 
-df_out.to_csv('State_Health_Summary.csv')
+df_out.to_csv('Gender_Summary.csv')
