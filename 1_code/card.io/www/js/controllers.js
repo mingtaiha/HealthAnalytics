b69ede38df3bd102fb45f99fa96305ebb1338e1c
itomaldonado <mo.maldonado@gmail.com>
2015-12-04 11:37:10 -0500

    Added string formatter

--- 1_code/card.io/www/js/controllers.js
+++ 1_code/card.io/www/js/controllers.js
@@ -939,10 +939,20 @@ angular.module('starter.controllers', ['ionic', 'starter.services'])
         .text(function(d) { return d.data.key; });
   }
 
-  $scope.formatPopulation = function (x) {
-    if(x !== '') {
-      return parseInt(x).toLocaleString();
-    }
+  $scope.formatPopulation = function (nStr) {
+    if(nStr !== '') {
+      nStr += '';
+      x = nStr.split('.');
+      x1 = x[0];
+      x2 = x.length > 1 ? '.' + x[1] : '';
+      
+      var rgx = /(\d+)(\d{3})/;
+      
+      while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+      }
+        return x1 + x2;
+      }
   }
 
 });
